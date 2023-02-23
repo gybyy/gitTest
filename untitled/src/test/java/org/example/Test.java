7b0f6292d6875c4dcef27ea1365bdc45daac4f9a
gyb <gyb123@163>
2023-02-23 15:23:00 +0800

    4 commit

--- untitled/src/test/java/org/example/Test.java
+++ untitled/src/test/java/org/example/Test.java
@@ -3,5 +3,7 @@ package org.example;
 public class Test {
     public static void main(String[] args) {
         System.out.println("这是一个git的案例的");
+        System.out.println("这是分支的情况的下离的");
+        System.out.println("我们要合并冲突的情况的下来的 ");
     }
 }
